03d9d31f6b565411ace4745e52a11ba2cb21b2c0
Rohan Wai Kit Sahgal <sahgalro@dh2020pc16.utm.utoronto.ca>
2020-09-30 17:39:24 -0400

    Simple command line argument parser adds host, port, and weight information to triples

--- proxy/MultiThreadedProxy.java
+++ proxy/MultiThreadedProxy.java
@@ -2,11 +2,42 @@
 import java.net.*;
 import java.io.*;
 import java.nio.*;
+import java.util.ArrayList;
 
         
 
 public class MultiThreadedProxy {
     public static void main(String[] args) {
+        
+        if ((args.length == 0) || (args.length % 3 != 0)){
+            throw new IllegalArgumentException("Wrong number of arguments!\nUsage: java proxy [remote port] [local port]");
+        }
+
+
+        
+        int num_triples = args.length / 3;
+
+        ArrayList<ProxyTriple> triples = new ArrayList<ProxyTriple>();
+
+        int j = 0;
+        while (j < args.length){
+            
+            String host;
+            int localport;
+            int weight;
+
+            try {
+                host = args[j++];
+                localport = Integer.parseInt(args[j++]);
+                weight = Integer.parseInt(args[j++]);
+                triples.add(new ProxyTriple(host, localport, weight));
+            } catch (NumberFormatException e) {
+                System.err.println("port or weight not a number");
+                System.exit(-1);
+            }
+            
+        }
+
         
         
         //Variable Declaration 
@@ -42,7 +73,9 @@ public class MultiThreadedProxy {
             System.err.println(e);
         }
 
-        
+        // hash short url
+        // 
 
     }
 }
+
