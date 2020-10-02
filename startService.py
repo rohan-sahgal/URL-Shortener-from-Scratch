827083a364bf5eff4352ae014fb17ae39cf2ef1b
Guang Yao Ma <maguang@dh2010pc30.utm.utoronto.ca>
2020-10-02 17:07:27 -0400

    Proxy, db and URL shortener service all spin up correctly now

--- startService.py
+++ startService.py
@@ -6,40 +6,65 @@ import subprocess, os
 class OrchestrationService(Cmd):
     prompt = '> '
     intro = "Orchestration Service for CSC409. Type ? to list commands"
-    port = 8000
+    PROXY_PORT = 8000
+    URL_SHORTENER_PORT = 8001
     
     def do_start(self, input):
-        # Start Service
+        '''# Setup/start the database, proxy, load balancers and URLShortener
+        service on all hosts defined in the `host` file.'''
+        
         CWD = os.getcwd()
 
         with open('hosts') as hosts_file:
+            firstHost = self.get_first_line(hosts_file).rstrip()
+            # Setup Proxy Server
+            print ("Starting up {} proxy server".format(firstHost))
+                            
+            subprocess.run(["ssh", firstHost, "cd {}/proxy; nohup java MultiThreadedProxy 8000 4 {} {} 1 > out/proxy{}.out, 2>out/proxyService{}.error < /dev/null &".format(CWD, firstHost, self.PROXY_PORT, firstHost, firstHost)])
+            
+            
             for host in hosts_file:
                 n = 1
                 host = host.rstrip()
                 # TODO: need to catch bad host names
-
+                
                 # Setup Database
-                # subprocess.run(f"ssh {host} \"cd {CWD}/dbpackage/; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' MakeDB url{n}.db jdbc:sqlite:/virtual/\"", shell=True)
+                print("Starting up {} database".format(host))
+                subprocess.run(["ssh", host, "cd {}/dbpackage/; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' MakeDB url{}.db jdbc:sqlite:/virtual/".format(CWD, n)], stdout=subprocess.DEVNULL)
+            
+                # Setup URL Shortener
+                print("Starting up {} URL Shortener service".format(host))
+                subprocess.run(["ssh", host, "cd {}; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' URLShortner {} url{}.db jdbc:sqlite:/virtual/ > out/shortenerService{}.out, 2>out/shortenerService{}.error < /dev/null &".format(CWD, self.URL_SHORTENER_PORT, n, host, host)])
                 
-                # Setup Proxy
-                subprocess.run(["ssh", host, "cd {}/proxy; nohup java MultiThreadedProxy 8000 4 {} {} 1".format(CWD, host, self.port)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                
-                break
                 
     def do_stop(self, input):
-        # Stop Database, proxy, and URL Shortening Services
+        '''# Stop the database, proxy, load balancers and URLShortener service 
+        on all hosts defined in the `host` file.'''
         CWD = os.getcwd()
         
-        # TODO: Not working (although the rm /virtual*.db works when running in a regular shell)
         with open('hosts') as hosts_file:
-            print("Shutting down databases...")
-            for host in hosts_file:
-                subprocess.run(f"ssh {host} \"rm /virtual/*.db;\"", shell=True)
-                break
-        
-        # Stop proxy server
-        # kill $(lsof -i -P | grep '8000' | cut -d" " -f5)
+            firstHost = self.get_first_line(hosts_file).rstrip()
+            # Stop proxy server
+            print("Shutting down {} proxy server...".format(firstHost))
+            subprocess.run(["ssh", firstHost, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.PROXY_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
 
+            for host in hosts_file:
+                host = host.rstrip()
+                
+                # Remove *.db file
+                print("Shutting down {} database...".format(host))
+                subprocess.run(["ssh", host, "cd {}; rm /virtual/*.db".format(CWD)])
+
+                # Stop URL Shortener
+                print ("Shutting down {} URL Shortener Service".format(host))
+                subprocess.run(["ssh", host, "kill $(lsof -i -P | grep 8080 | cut -d' ' -f5)"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+                
+                
+    def get_first_line(self, file):
+        pos = file.tell()
+        line = file.readline()
+        file.seek(pos)
+        return line
 
     def do_exit(self, input):
         '''Exit the service.'''
@@ -47,3 +72,4 @@ class OrchestrationService(Cmd):
 
 
 OrchestrationService().cmdloop()
+
