10a2ddb82d2a30af8952fa7e2cfb4830474d481e
Guang Yao Ma <maguang@dh2010pc30.utm.utoronto.ca>
2020-10-02 02:41:15 -0400

    In progress, need to rewrite database close and proxy close

--- startService.py
+++ startService.py
@@ -6,6 +6,7 @@ import subprocess, os
 class OrchestrationService(Cmd):
     prompt = '> '
     intro = "Orchestration Service for CSC409. Type ? to list commands"
+    port = 8000
     
     def do_start(self, input):
         # Start Service
@@ -15,24 +16,32 @@ class OrchestrationService(Cmd):
             for host in hosts_file:
                 n = 1
                 host = host.rstrip()
-                
-                print(CWD)
+                # TODO: need to catch bad host names
 
                 # Setup Database
-                subprocess.run(f"ssh {host} \"cd {CWD}/dbpackage/; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' MakeDB url{n}.db jdbc:sqlite:/virtual/\"", shell=True)
+                # subprocess.run(f"ssh {host} \"cd {CWD}/dbpackage/; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' MakeDB url{n}.db jdbc:sqlite:/virtual/\"", shell=True)
+                
+                # Setup Proxy
+                subprocess.run(["ssh", host, "cd {}/proxy; nohup java MultiThreadedProxy 8000 4 {} {} 1".format(CWD, host, self.port)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                
                 break
                 
     def do_stop(self, input):
         # Stop Database, proxy, and URL Shortening Services
         CWD = os.getcwd()
-
+        
+        # TODO: Not working (although the rm /virtual*.db works when running in a regular shell)
         with open('hosts') as hosts_file:
             print("Shutting down databases...")
             for host in hosts_file:
-                subprocess.run(f"ssh {host} \"rm /virtual/*.db\"", shell=True)
+                subprocess.run(f"ssh {host} \"rm /virtual/*.db;\"", shell=True)
                 break
+        
+        # Stop proxy server
+        # kill $(lsof -i -P | grep '8000' | cut -d" " -f5)
 
-    def do_exit(self):
+
+    def do_exit(self, input):
         '''Exit the service.'''
         return True
 
