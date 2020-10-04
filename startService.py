820409d4acacf229bbff22403e7ab5db618af6f3
Guang Yao Ma <maguang@dh2010pc30.utm.utoronto.ca>
2020-10-03 20:03:42 -0400

    Create outfiles in python startScript and add a java compile script

--- startService.py
+++ startService.py
@@ -10,9 +10,13 @@ class OrchestrationService(Cmd):
     URL_SHORTENER_PORT = 8001
     LOAD_BALANCER_PORT = 8002
     
+    # TODO: need to mkdir the corresponding out folders or add the out folders to the repo
     def do_start(self, input):
         '''# Setup/start the database, proxy, load balancers and URLShortener
         service on all hosts defined in the `host` file.'''
+        
+        # Create out directories
+        self.make_output_directories()
         
         CWD = os.getcwd()
         
@@ -24,7 +28,6 @@ class OrchestrationService(Cmd):
         
         if len(hosts_array) == 0:
             raise Exception('Error: No hosts specified in the host file.')
-
         
         argsLB, argsProxy = "", ""
         for host in hosts_array:
@@ -56,7 +59,8 @@ class OrchestrationService(Cmd):
             print("Starting up {} URL Shortener service".format(host))
             subprocess.run(["ssh", host, "cd {}/dbpackage; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' URLShortner {} url{}.db jdbc:sqlite:/virtual/ > out/shortenerService{}.out 2>out/shortenerService{}.error < /dev/null &".format(CWD, self.URL_SHORTENER_PORT, n, host, host)])
                 
-                
+            n += 1
+
     def do_stop(self, input):
         '''# Stop the database, proxy, load balancers and URLShortener service 
         on all hosts defined in the `host` file.'''
@@ -82,7 +86,19 @@ class OrchestrationService(Cmd):
                 # Stop Load Balancer
                 print ("Shutting down {} Load Balancer".format(host))
                 subprocess.run(["ssh", host, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.LOAD_BALANCER_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
-                
+    
+    def make_output_directories(self):
+        PROXY_OUT_DIR = "proxy/out"
+        DBPACKAGE_OUT_DIR = "dbpackage/out"
+          
+        try:
+          if not os.path.exists(PROXY_OUT_DIR):
+              os.mkdir(PROXY_OUT_DIR)
+          
+          if not os.path.exists(DBPACKAGE_OUT_DIR):
+              os.mkdir(DBPACKAGE_OUT_DIR)
+        except OSError:
+          print("Creation of the directories failed")                    
                 
     def get_first_line(self, file):
         pos = file.tell()
