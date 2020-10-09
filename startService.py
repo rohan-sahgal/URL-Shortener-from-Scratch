60b4121a7c051232348c8075c056b2c5763513fa
Guang Yao Ma <maguang@dh2020pc10.utm.utoronto.ca>
2020-10-09 17:36:03 -0400

    Cleaned up orchestration script

--- startService.py
+++ startService.py
@@ -4,12 +4,26 @@ from cmd import Cmd
 import subprocess, os
 
 class OrchestrationService(Cmd):
+
+    def __init__(self):
+        super(OrchestrationService, self).__init__()
+    
+        # Read file into an array once.
+        self.hosts_array = []
+        with open('hosts') as hosts_file:
+            for host in hosts_file:
+                self.hosts_array.append(host.rstrip())
+                
+        if len(self.hosts_array) == 0:
+            raise Exception('Error: No hosts specified in the host file.')
+        
     prompt = '> '
     intro = "Orchestration Service for CSC409. Type ? to list commands"
-    PROXY_PORT = 8000
-    URL_SHORTENER_PORT = 8001
-    LOAD_BALANCER_PORT = 8002
     
+    PROXY_PORT = 8000
+    LOAD_BALANCER_PORT = 8001
+    URL_SHORTENER_PORT = 8002
+
     # TODO: need to mkdir the corresponding out folders or add the out folders to the repo
     def do_start(self, input):
         '''# Setup/start the database, proxy, load balancers and URLShortener
@@ -18,31 +32,25 @@ class OrchestrationService(Cmd):
         # Create out directories
         self.make_output_directories()
         
+        # Compile java files
+        self.compile_java_files()
+        
         CWD = os.getcwd()
         
-        # Read file into an array once.
-        hosts_array = []
-        with open('hosts') as hosts_file:
-            for host in hosts_file:
-                hosts_array.append(host.rstrip())
-        
-        if len(hosts_array) == 0:
-            raise Exception('Error: No hosts specified in the host file.')
-        
         argsLB, argsProxy = "", ""
-        for host in hosts_array:
+        for host in self.hosts_array:
             host = host.rstrip()
             
             argsLB += host + " " + str(self.URL_SHORTENER_PORT) + " " + "1" + " "
             argsProxy += host + " " + str(self.LOAD_BALANCER_PORT) + " " + "1" + " "
 
-        firstHost = hosts_array[0]
+        firstHost = self.hosts_array[0]
         
         # Setup Proxy Server
         print ("Starting up {} proxy server".format(firstHost))
         subprocess.run(["ssh", firstHost, "cd {}/proxy; nohup java MultiThreadedProxy {} 4 {} > out/proxy{}.out 2>out/proxy{}.error < /dev/null &".format(CWD, self.PROXY_PORT, argsProxy, firstHost, firstHost)])
         
-        for host in hosts_array:
+        for host in self.hosts_array:
             n = 1
             host = host.rstrip()
             # TODO: need to catch bad host names
@@ -66,26 +74,29 @@ class OrchestrationService(Cmd):
         on all hosts defined in the `host` file.'''
         CWD = os.getcwd()
         
-        with open('hosts') as hosts_file:
-            firstHost = self.get_first_line(hosts_file).rstrip()
-            # Stop proxy server
-            print("Shutting down {} proxy server...".format(firstHost))
-            subprocess.run(["ssh", firstHost, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.PROXY_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+        firstHost = self.hosts_array[0]
+        # Stop proxy server
+        print("Shutting down {} proxy server...".format(firstHost))
+        subprocess.run(["ssh", firstHost, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.PROXY_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
 
-            for host in hosts_file:
-                host = host.rstrip()
-                
-                # Remove *.db file
-                print("Shutting down {} database...".format(host))
-                subprocess.run(["ssh", host, "cd {}; rm /virtual/*.db".format(CWD)])
+        for host in self.hosts_array:
+            host = host.rstrip()
+            
+            # Remove *.db file
+            print("Shutting down {} database...".format(host))
+            subprocess.run(["ssh", host, "cd {}; rm /virtual/*.db".format(CWD)])
 
-                # Stop URL Shortener
-                print ("Shutting down {} URL Shortener Service".format(host))
-                subprocess.run(["ssh", host, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.URL_SHORTENER_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
-                
-                # Stop Load Balancer
-                print ("Shutting down {} Load Balancer".format(host))
-                subprocess.run(["ssh", host, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.LOAD_BALANCER_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+            # Stop URL Shortener
+            print ("Shutting down {} URL Shortener Service".format(host))
+            subprocess.run(["ssh", host, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.URL_SHORTENER_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+            
+            # Stop Load Balancer
+            print ("Shutting down {} Load Balancer".format(host))
+            subprocess.run(["ssh", host, "kill $(lsof -i -P | grep {} | cut -d' ' -f5)".format(self.LOAD_BALANCER_PORT)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) 
+    
+    def compile_java_files(self):
+        print ("Compiling java executables")
+        subprocess.run("./compileJava", shell=True)
     
     def make_output_directories(self):
         PROXY_OUT_DIR = "proxy/out"
@@ -100,12 +111,6 @@ class OrchestrationService(Cmd):
         except OSError:
           print("Creation of the directories failed")                    
                 
-    def get_first_line(self, file):
-        pos = file.tell()
-        line = file.readline()
-        file.seek(pos)
-        return line
-
     def do_exit(self, input):
         '''Exit the service.'''
         return True
