30f09ad8216d836a6b1ba61af1e03fc2486d247a
Guang Yao Ma <maguang@dh2010pc37.utm.utoronto.ca>
2020-10-10 22:21:41 -0400

    Removed a bunch of boilerplate gnu and testing, added gnu plots

--- performanceTesting/readTest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import random, string, subprocess
-
-for i in range(10000):
-	request="http://localhost:8080/000000000000000000000000000000000000000000000"
-	# print(request)
-	subprocess.call(["curl", "-X", "GET", request], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
