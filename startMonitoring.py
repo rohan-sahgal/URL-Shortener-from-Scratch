8ab489525d032144dddc73a61e35773a30b88a77
Rohan <rohan.sahgal@mail.utoronto.ca>
2020-10-10 23:41:46 -0400

    Fixed broken monitoring - ports now gotten from hosts file

--- startMonitoring.py
+++ startMonitoring.py
@@ -5,9 +5,10 @@ import subprocess, os, time
 import signal
 import sys
 
-PROXY_PORT = 8000
-LOAD_BALANCER_PORT = 8001
-URL_SHORTENER_PORT = 8002
+PROXY_PORT = 0
+LOAD_BALANCER_PORT = 0
+URL_SHORTENER_PORT = 0
+CACHE_SIZE = 0
 
 hosts_array = []
 hosts_ranges_start = []
@@ -23,19 +24,37 @@ def signal_handler(sig, frame):
 
 
 def init_hosts(hosts):
-
+    global PROXY_PORT
+    global LOAD_BALANCER_PORT
+    global URL_SHORTENER_PORT
+    global CACHE_SIZE
     with open('hosts') as hosts_file:
-        for host in hosts_file:
-            host_range = host.rstrip().split(" ")
-            hosts_array.append(host_range[0])
-            hosts_ranges_start.append(host_range[1])
-            hosts_ranges_end.append(host_range[2])
-            
+        i = 0
+        for line in hosts_file:
+
+            if (i == 0):
+                port_line = line.rstrip().split(" ")
+                PROXY_PORT = (int(port_line[0]))
+                LOAD_BALANCER_PORT = (int(port_line[1]))
+                URL_SHORTENER_PORT = (int(port_line[2]))
+            elif (i == 1):
+                CACHE_SIZE = (int(line.rstrip()))
+            else:
+                host_range = line.rstrip().split(" ")
+                hosts_array.append(host_range[0])
+                hosts_ranges_start.append(host_range[1])
+                hosts_ranges_end.append(host_range[2])
+                if (host_range[1] == '0' and host_range[2] == '255'):
+                    range_exists = True
+            i += 1
+
+    if range_exists == False:
+        raise Exception('Error: No backup host specified')
     if len(hosts) == 0:
         raise Exception('Error: No hosts specified in the host file.')
 
     firstHost = hosts[0]
-    
+
 
 
 def revive(serviceName, hostName, servicePort):
@@ -47,12 +66,15 @@ def revive(serviceName, hostName, servicePort):
         subprocess.run(["ssh", hostName, "cd {}/proxy; nohup java MultiThreadedLB {} 4 {} > out/LB{}.out 2>out/LB{}.error < /dev/null &".format(CWD, servicePort, argsLB, hostName, hostName)])
         print ("Successfully restarted {} on {}:{}".format(serviceName, hostName, servicePort))
     elif serviceName == "URL Shortener":
-        subprocess.run(["ssh", hostName, "cd {}/dbpackage; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' URLShortner {} url{}.db jdbc:sqlite:/virtual/ > out/shortenerService{}.out 2>out/shortenerService{}.error < /dev/null &".format(CWD, servicePort, hosts_array.index(hostName) + 1, hostName, hostName)])
+        subprocess.run(["ssh", hostName, "cd {}/dbpackage; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' URLShortner {} {} url{}.db jdbc:sqlite:/virtual/ > out/shortenerService{}.out 2>out/shortenerService{}.error < /dev/null &".format(CWD, servicePort, CACHE_SIZE, hosts_array.index(hostName) + 1, hostName, hostName)])
+        # subprocess.run(["ssh", hostName, "cd {}/dbpackage; java -classpath '.:../db/sqlite-jdbc-3.32.3.2.jar' URLShortner {} url{}.db jdbc:sqlite:/virtual/ > out/shortenerService{}.out 2>out/shortenerService{}.error < /dev/null &".format(CWD, servicePort, hosts_array.index(hostName) + 1, hostName, hostName)])
         print ("Successfully restarted {} on {}:{}".format(serviceName, hostName, servicePort))
 
 def service_status(serviceName, hostName, servicePort, proxyOutput):
     if proxyOutput == b'':
         revive(serviceName, hostName, servicePort)
+
+
 
 
 init_hosts(hosts_array)
@@ -70,6 +92,7 @@ signal.signal(signal.SIGINT, signal_handler)
 
 # Main Loop
 print("Monitoring services that go down...\nPress Ctrl+C to exit.")
+
 while True:
 
     proxyOutput = subprocess.run(["ssh", hosts_array[0], "lsof -i -P | grep {} | cut -d' ' -f5".format(PROXY_PORT)], stdout=PIPE, stderr=PIPE)
